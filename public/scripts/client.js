252effe7a9caf3ed68ac8d47719b1e560e3e83e8
Saad Kureshi <saq619@gmail.com>
2020-12-17 04:28:22 +0000

    Core functionality updated

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,37 +1,11 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
- // Test / driver code (temporary). Eventually will get this from the server.
-
 $(document).ready(function(){
-// Fake data taken from initial-tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
+
+  $("#write-new-tweet-btn").on("click", function() {
+    $("#submit-tweet").slideToggle(function(){
+      $("#tweet-text").focus();
+      $("#nav-btn-icon").toggleClass("fa-angle-double-down fa-angle-double-up")
+    });
+  });
 
   function createTweetElement(tweetObj){
   const $tweet = `        
@@ -41,8 +15,8 @@ $(document).ready(function(){
       <span class="username-in-tweet">${tweetObj.user.name}</span>
       <span class="user-handle">${tweetObj.user.handle}</span>
     </header>
-    <p>
-      ${tweetObj.content.text}
+    <p id="the-actual-tweet">
+      ${escape(tweetObj.content.text)}
     </p>
     <footer class="each-tweet-footer">
       <span class="tweet-post-date">${getDate(tweetObj.created_at)}</span>
@@ -60,16 +34,85 @@ $(document).ready(function(){
     let tweet = '';
     for (let tweetObj of tweetObjArray) {
       tweet = createTweetElement(tweetObj);
-      $(".posted-tweets").append(tweet);
+      $(".posted-tweets").prepend(tweet);
       tweet = '';
     };
   };
+
+  const escape = function(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
 
   function getDate(milliseconds) {
     return new Date(milliseconds).toDateString();
   }
 
-  renderTweets(data);
+  $("#submit-tweet").on('submit', function(e) {
+    e.preventDefault();
+
+    const $tweetContent = $(this).children("#tweet-text").val();
+    const $counterValue = $(this).find(".counter").val();
+    const $errorMsg = $(".error-msg-box").children(".error-msg");
+
+    if ($(".error-msg-box").css('display') !== "none") {
+      $(".error-msg-box").slideUp("fast");
+    }
+
+    if ($tweetContent === "") {
+      $errorMsg.text("The input field is empty.");
+      $(".error-msg-box").slideDown("fast");
+      return;
+    } else if ($counterValue < 0) {
+      $errorMsg.text("Tweets cannot exceed 140 characters.");
+      $(".error-msg-box").slideDown("fast");
+      return;
+    }
+    $.ajax({
+        method: "POST", 
+        url: "/tweets", 
+        data: $(this).serialize()
+      })
+        .then(() => {
+          $(this).children("#tweet-text").val("");
+          $(this).find(".counter").val(140);
+          $(".posted-tweets").empty();
+          loadTweets();
+        })
+        .catch(err => console.log(err))
+  });
+
+  // Alt way of sending $.ajax request:
+  // $("#submit-tweet").on('submit', function(e) {
+  //   e.preventDefault();
+  //   const $tweetBox = $(this).children("#tweet-text");
+  //   const tweetContent = $tweetBox.val();
+  //   console.log(tweetContent);
+  // $.ajax({
+  //   method: "POST", 
+  //   url: "/tweets", 
+  //   data: {text: tweetContent}
+  //   // data: $(this).serialize()
+  // })
+  //   .then(response => {
+  //     console.log(response);
+  // })
+  //   .catch(err => console.log(err))
+
+  function loadTweets() {
+    $.ajax({
+      method: "GET",
+      url: "/tweets",
+      dataType: "json"
+    })
+    .then(response => {
+      renderTweets(response);
+    })
+    .catch(err => console.log(err));
+  };
+
+  loadTweets();
 
 });
 
